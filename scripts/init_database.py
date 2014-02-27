53a15c1c47360041cfec2ff95d0cf9a02ca7f871
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2014-02-27 14:22:40 -0500

    Ensure init works on different hosts

--- scripts/init_database.py
+++ scripts/init_database.py
@@ -1,10 +1,17 @@
 from geminiutil.gmos import GMOSMOSProject
 
+import socket
+host = socket.gethostname()
+if host == 'swan':
+    work_dir = '/work/mhvk/mcsnr/'
+elif host == 'moria':
+    work_dir = '/media/data1/mcsnr/'
+else:
+    raise ValueError("Unknown host='{0}'".format(host))
 
 dbname = 'sqlite:///databases/gmos.db3'
 
-proj = GMOSMOSProject(dbname, work_dir='/media/data1/mcsnr/', echo=False)
-
+proj = GMOSMOSProject(dbname, work_dir=work_dir, echo=False)
 proj.initialize_database()
 proj.add_directory('gmos_data/raw')
 proj.add_directory('gmos_data/mdf_dir')
