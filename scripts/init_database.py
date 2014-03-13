47a94a1d2172384911ceb8a2f68f896619c4f558
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-03-13 15:41:29 -0400

    changed the scripts to point database to gmos_working_copy.db3

--- scripts/init_database.py
+++ scripts/init_database.py
@@ -1,5 +1,5 @@
 from geminiutil.gmos import GMOSMOSProject
-
+import os
 import socket
 host = socket.gethostname()
 if host == 'swan':
@@ -7,10 +7,14 @@ if host == 'swan':
     dbname = 'sqlite:///gmos.db3'
 elif host == 'moria':
     work_dir = '/media/data1/mcsnr/'
-    dbname = 'sqlite:///databases/gmos.db3'
+    dbname = 'sqlite:///databases/gmos_working_copy.db3'
 else:
     raise ValueError("Unknown host='{0}'".format(host))
 
+
+os.system('rm {0}'.format(db_fname))
+
+gmos_working_copy.db3
 
 proj = GMOSMOSProject(dbname, work_dir=work_dir, echo=False)
 proj.initialize_database()
